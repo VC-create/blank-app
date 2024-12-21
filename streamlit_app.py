0d68cf151e2258c656192fa330e844c67b1daac9
VC-create <vedika.ar.chaudhari@gmail.com>
2024-12-21 16:41:03 +0000

    original commit of coded soundboard app

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,23 @@
 import streamlit as st
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+def button_click(col):
+    speechContainer.audio(speechArr[col], format='audio/mpeg')
+
+st.title("Soundboard App")
+st.subheader(
+    "Click on a person to hear their famous speech"
 )
+
+names = ['Martin Luther King Jr','Kamala Harris']
+imgArr = ['mlk.jpg', 'kamalaharris.png']
+speechArr = ['mlk.mp3', 'kamala.mp3']
+
+cols=st.columns(2)
+for c in range(0, len(names)):
+    cols[c].image(imgArr[c])
+    cols[c].button(names[c], on_click=button_click, args=(c,),use_container_width=True)
+
+speechContainer=st.container(border=True)
+speechContainer.write('Audio: ')
+
+
